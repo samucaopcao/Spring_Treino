6f809ab9fe61e9d2aa8dc2f330ab279295ae3927
Samuel Macedo <samuelmacedo2011@gmail.com>
2021-05-01 20:18:56 -0300

    Mudança de base de dados para PostGres

---
+++ src/main/java/com/example/demo/model/Rule.java
@@ -0,0 +1,65 @@
+package com.example.demo.model;
+
+import java.io.Serializable;
+
+public class Rule implements Serializable {
+
+	/**
+	 * Classe Responsável Pelas Regras de Permissão
+	 */
+	private static final long serialVersionUID = 1L;
+	private Long id;
+	private String authority;
+
+	public Rule() {
+		super();
+	}
+
+	public Rule(Long id, String authority) {
+		super();
+		this.id = id;
+		this.authority = authority;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getAuthority() {
+		return authority;
+	}
+
+	public void setAuthority(String authority) {
+		this.authority = authority;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Rule other = (Rule) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+}
